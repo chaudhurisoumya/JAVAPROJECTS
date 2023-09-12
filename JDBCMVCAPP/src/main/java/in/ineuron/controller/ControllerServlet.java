43ef8cc9d2d642c14125e032cbc35cab1f749517
Soumyadip Chaudhuri <137050154+chaudhurisoumya@users.noreply.github.com>
2023-09-12 21:12:35 +0530

    Add files via upload

--- JDBCMVCAPP/src/main/java/in/ineuron/controller/ControllerServlet.java
+++ JDBCMVCAPP/src/main/java/in/ineuron/controller/ControllerServlet.java
@@ -1,7 +1,6 @@
 package in.ineuron.controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
@@ -98,32 +97,8 @@ public class ControllerServlet extends HttpServlet {
 			String sid = request.getParameter("sid");
 			Student student = stdService.findById(Integer.parseInt(sid));
 			if (student != null) {
-				response.setContentType("text/html");
-
-				// display editpage using html
-				PrintWriter out = response.getWriter();
-				out.println("<html><head><title>OUTPUT</title></head>");
-				out.println("<body bgcolor='lightblue'>");
-				out.println("<br/><br/><br/>");
-				out.println("<form method='post' action='./update'>");
-				out.println("<table align='center'>");
-				out.println("<tr><th>ID</th><td>" + student.getSid() + "</td></tr>");
-				out.println("<input type='hidden' name='sid' value='" + student.getSid() + "'/>");
-				out.println("<tr><th>NAME</th><td><input type='text' name='sname' value='" + student.getSname()
-						+ "'/></td></tr>");
-				out.println("<tr><th>AGE</th><td><input type='text' name='sage' value='" + student.getSage()
-						+ "'/></td></tr>");
-				out.println("<tr><th>ADDRESS</th><td><input type='text' name='saddr' value='" + student.getSaddr()
-						+ "'/></td></tr>");
-				out.println("<tr><td></td><td><input type='submit' value='update'/></td></tr>");
-				out.println("</table>");
-				out.println("</form>");
-				out.println("</body>");
-				out.println("</html>");
-				out.close();
-
-			} else {
-				rd = request.getRequestDispatcher("../notfound.html");
+				request.setAttribute("student", student);
+				rd = request.getRequestDispatcher("../edit.jsp");
 				rd.forward(request, response);
 			}
 		}
@@ -143,10 +118,8 @@ public class ControllerServlet extends HttpServlet {
 
 			String status = stdService.updateById(student);
 			if (status.equals("success")) {
-				rd = request.getRequestDispatcher("../success.html");
-				rd.forward(request, response);
-			} else if (status.equals("failure")) {
-				rd = request.getRequestDispatcher("../failure.html");
+				request.setAttribute("status", status);
+				rd = request.getRequestDispatcher("../../disp.jsp");
 				rd.forward(request, response);
 			}
 		}
